d051925f350440d20c39be9c6d132e7467d82871
GeniusX7 <948140863@qq.com>
2017-07-13 16:23:13 +0800

    wcf example files
    first simple example

---
+++ Client/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ChannelFactory<ICalculator> channelFactory =
+                /*new ChannelFactory<ICalculator>(new WSHttpBinding(), "Http://127.0.0.1:1234/calculatorservice")*/
+                new ChannelFactory<ICalculator>("calculatorserviceok")
+                )
+            {
+                ICalculator proxy = channelFactory.CreateChannel();
+                Console.WriteLine("x+y={0}", proxy.Add(1, 2));
+                Console.WriteLine("x-y={0}", proxy.Subtract(1, 2));
+                Console.WriteLine("x*y={0}", proxy.Multiply(4, 2));
+                Console.WriteLine("x/y={0}", proxy.Divide(1, 2));
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Host/Program.cs
@@ -0,0 +1,39 @@
+﻿using Service;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(CalculatorService)))
+            {
+                //host.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(),
+                //    "Http://127.0.0.1:1234/calculatorservice");
+                //if (host.Description.Behaviors.Find<ServiceMetadataBehavior>() == null)
+                //{
+                //    ServiceMetadataBehavior behavior = new ServiceMetadataBehavior();
+                //    behavior.HttpGetEnabled = true;
+                //    behavior.HttpGetUrl = new Uri("Http://127.0.0.1:1234/calculatorservice/metadata");
+                //    host.Description.Behaviors.Add(behavior);
+                //}
+
+                host.Opened += delegate
+                {
+                    Console.WriteLine("CalcultorService is opend, press any key to exit...");
+                };
+
+                host.Open();
+                Console.Read();
+            }
+        }
+    }
+}
